daa45c3347b5cd6fcc9a7c244b6a670d0cb54d05
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-14 13:08:05 +0530

    styling changes

--- modal/src/Modal.jsx
+++ modal/src/Modal.jsx
@@ -4,17 +4,47 @@ import "./Modal.css";
 const Modal = () => {
   const [shoModal, setShowModal] = useState(false);
   return (
-    <div className="container">
-      <button onClick={() => setShowModal(true)}>Click me</button>
+    <>
+      <div
+        className="container"
+        style={shoModal ? { opacity: 0.5 } : { opacity: 1 }}
+      >
+        <button onClick={() => setShowModal(true)}>Click me</button>
+        <p>
+          Irure dolor dolor duis laboris adipisicing est ipsum id amet dolor
+          sint. Irure dolor dolor duis laboris adipisicing est ipsum id amet
+          dolor sint.
+        </p>
+        <p>
+          Irure dolor dolor duis laboris adipisicing est ipsum id amet dolor
+          sint. Irure dolor dolor duis laboris adipisicing est ipsum id amet
+          dolor sint.
+        </p>
+        <p>
+          Irure dolor dolor duis laboris adipisicing est ipsum id amet dolor
+          sint. Irure dolor dolor duis laboris adipisicing est ipsum id amet
+          dolor sint.
+        </p>
+        <p>
+          Irure dolor dolor duis laboris adipisicing est ipsum id amet dolor
+          sint. Irure dolor dolor duis laboris adipisicing est ipsum id amet
+          dolor sint.
+        </p>
+        <p>
+          Irure dolor dolor duis laboris adipisicing est ipsum id amet dolor
+          sint. Irure dolor dolor duis laboris adipisicing est ipsum id amet
+          dolor sint.
+        </p>
+      </div>
       {shoModal && (
-        <div className="modal">
+        <div className="modal" style={{ opacity: 1 }}>
           <h1>Heading</h1>
           Irure dolor dolor duis laboris adipisicing est ipsum id amet dolor
           sint. Irure dolor dolor duis laboris adipisicing est ipsum id amet
           dolor sint.
         </div>
       )}
-    </div>
+    </>
   );
 };
 
