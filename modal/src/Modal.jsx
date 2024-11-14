ee32e2d05b4dce30e251a6948ad4ff532ceb1f45
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-14 13:01:33 +0530

    added modal

--- modal/src/Modal.jsx
+++ modal/src/Modal.jsx
@@ -2,10 +2,18 @@ import { useState } from "react";
 import "./Modal.css";
 
 const Modal = () => {
-  cosnt[(shoModal, setShowModal)] = useState(false);
+  const [shoModal, setShowModal] = useState(false);
   return (
     <div className="container">
-      <button>Click me</button>
+      <button onClick={() => setShowModal(true)}>Click me</button>
+      {shoModal && (
+        <div className="modal">
+          <h1>Heading</h1>
+          Irure dolor dolor duis laboris adipisicing est ipsum id amet dolor
+          sint. Irure dolor dolor duis laboris adipisicing est ipsum id amet
+          dolor sint.
+        </div>
+      )}
     </div>
   );
 };
