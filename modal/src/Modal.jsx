6a200fc1a823c9b8321a9d6f263ed16fbe6f9732
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-14 12:57:16 +0530

    added state

--- modal/src/Modal.jsx
+++ modal/src/Modal.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./Modal.css";
 
 const Modal = () => {
+  cosnt[(shoModal, setShowModal)] = useState(false);
   return (
     <div className="container">
       <button>Click me</button>
